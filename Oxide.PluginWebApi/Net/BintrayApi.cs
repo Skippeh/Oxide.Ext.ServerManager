d93c2b70a3031f89b62bc0f10e9c739ff2b9fedc
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-05-15 17:50:13 +0200

    Bintray api progress

---
+++ Oxide.PluginWebApi/Net/BintrayApi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Oxide.PluginWebApi.Net.Models.Bintray;
+
+namespace Oxide.PluginWebApi.Net
+{
+    public class BintrayApi : IDisposable
+    {
+        private const string ApiUrl = "https://api.bintray.com/";
+        private const string FilesPath = "packages/oxidemod/builds/Oxide/files";
+        
+        private readonly WebClient webClient; // Don't need cookies for this
+
+        public BintrayApi(string username, string apiKey)
+        {
+            webClient = new WebClient();
+            string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + apiKey));
+            webClient.Headers[HttpRequestHeader.Authorization] = $"Basic {credentials}";
+        }
+
+        public void Dispose()
+        {
+            webClient.Dispose();
+        }
+
+        public async Task<File[]> GetFiles()
+        {
+            return JsonConvert.DeserializeObject<File[]>(await webClient.DownloadStringTaskAsync(ApiUrl + FilesPath));
+        }
+    }
+}
---
+++ Oxide.PluginWebApi/Net/Models/Bintray/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Oxide.PluginWebApi.Net.Models.Bintray
+{
+    public class File
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Repo { get; set; }
+        public string Package { get; set; }
+        public string Version { get; set; }
+        public string Owner { get; set; }
+        public DateTime Created { get; set; }
+        public int Size { get; set; }
+        public string Sha1 { get; set; }
+        public string Sha256 { get; set; }
+    }
+}
