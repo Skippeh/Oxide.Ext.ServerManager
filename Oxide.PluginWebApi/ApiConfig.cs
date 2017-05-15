012f2a763c944b1ccb43311d9df56200b3f71abc
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-05-15 12:51:47 +0200

    Ignore loading OxideUsername and OxidePassword if values are null

--- Oxide.PluginWebApi/ApiConfig.cs
+++ Oxide.PluginWebApi/ApiConfig.cs
@@ -1,11 +1,16 @@
-﻿using Oxide.PluginWebApi.Configuration;
+﻿using Newtonsoft.Json;
+using Oxide.PluginWebApi.Configuration;
 
 namespace Oxide.PluginWebApi
 {
     public class ApiConfig : ConfigFile
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string OxideUsername { get; set; } = "";
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string OxidePassword { get; set; } = "";
+
         public int Port { get; set; } = 8090;
     }
 }
