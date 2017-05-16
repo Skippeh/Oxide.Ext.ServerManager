5169c82bd982d776e5aa7b4ac63b8fd1bb27f1cd
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-05-16 14:05:24 +0200

    Implement OxideApi.Authenticate

--- Oxide.PluginWebApi/Net/OxideApi.cs
+++ Oxide.PluginWebApi/Net/OxideApi.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace Oxide.PluginWebApi.Net
 {
@@ -15,11 +17,30 @@ namespace Oxide.PluginWebApi.Net
 
         public static bool Authenticate(string username, string password)
         {
-            
+            using (var webClient = new CookieWebClient(cookieContainer))
+            {
+                byte[] responseBytes = webClient.UploadValues("http://oxidemod.org/login/login", "POST", new NameValueCollection
+                {
+                    { "login", username },
+                    { "password", password }
+                });
 
-            return false;
+                string response = Encoding.UTF8.GetString(responseBytes);
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(response);
+                var rootNode = htmlDoc.DocumentNode;
+                
+                var content = rootNode.SelectSingleNode("//*[@id=\"content\"]");
+
+                if (content.GetAttributeValue("class", "").Contains("error_with_login"))
+                {
+                    return false;
+                }
+
+                return true;
+            }
         }
-
+        
         public OxideApi()
         {
             webClient = new CookieWebClient(cookieContainer);
